84171ba2bd470fc0e23458a96769890389d37720
Daniel <d.santamaria@uniandes.edu.co>
2019-10-25 13:00:57 -0500

    Villa Laura

---
+++ fincaVillaLaura/js/efectos.js
@@ -0,0 +1,18 @@
+$('.ir-arriba').click(function(){
+  $('body, html').animate({
+    scrollTop:'0px'
+  }, 300);
+});
+
+$(window).scroll(function(){
+  if($(this).scrollTop()>0){
+    $('.ir-arriba').slideDown(300);
+  }else{
+    $('.ir-arriba').slideUp(300);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  const imgLigthBox = document.querySelectorAll('.materialboxed');
+  M.Materialbox.init(imgLigthBox);
+});
