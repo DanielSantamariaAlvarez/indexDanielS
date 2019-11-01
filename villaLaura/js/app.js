91f8dcb028689707b31529ae645c5cb8f359c2c2
Daniel <d.santamaria@uniandes.edu.co>
2019-11-01 14:18:17 -0500

    Villa Laura nuevo

---
+++ villaLaura/js/app.js
@@ -0,0 +1,17 @@
+const seccionesPagina = new fullpage('#fullpage',{
+  autoScrolling: true,
+  fitToSection: false,
+  fitToSectionDelay: 300,
+  easing: 'easeInOutCubic',
+  scrollingSpeed: 700,
+  css3: true,
+  easingcss3: 'ease-out',
+  loopBottom: false,
+  navigation: true,
+  menu: '#menu',
+  anchors: ['inicio', 'servicios','ubicacion', 'contacto'],
+  navigationTooltips: ['Inicio', 'Productos', 'Contacto'],
+  showActiveTooltip: false,
+  sectionsColor : ['#000', '#c2c2c2','#c2c2c2' , '#000'],
+  verticalCentered: true,
+});
